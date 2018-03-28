50569c98010437e9679e4e1f07ef08190a065c27
Unknown <alexandre_wet@hotmail.com>
2018-03-28 14:15:38 +0200

    Rajout n Queen

--- src/WithChoco/NQueensChoco.java
+++ src/WithChoco/NQueensChoco.java
@@ -3,21 +3,22 @@ package WithChoco;
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solution;
 import org.chocosolver.solver.Solver;
-import org.chocosolver.solver.search.strategy.Search;
 import org.chocosolver.solver.variables.IntVar;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class NQueensChoco {
 
     public static void main(String[] args) {
         System.out.println("N-Queens");
-        solveNQueens(8);
+        solveNQueens(4);
     }
 
     //ONLINE SOLUTION
     static void solveNQueens(int n) {
         Model model = new Model(n + "-queens problem");
+        long start = System.currentTimeMillis();
         IntVar[] vars = model.intVarArray("Q", n, 1, n, false);
         IntVar[] diag1 = IntStream.range(0, n).mapToObj(i -> vars[i].sub(i).intVar()).toArray(IntVar[]::new);
         IntVar[] diag2 = IntStream.range(0, n).mapToObj(i -> vars[i].add(i).intVar()).toArray(IntVar[]::new);
@@ -27,11 +28,10 @@ public class NQueensChoco {
                 model.allDifferent(diag2)
         );
         Solver solver = model.getSolver();
-        solver.setSearch(Search.domOverWDegSearch(vars));
-        Solution solution = solver.findSolution();
-        if (solution != null) {
-            System.out.println(solution);
-        }
+        List<Solution> solutions = solver.findAllSolutions();
+        long end = System.currentTimeMillis();
+        System.out.println(solutions);
+        System.out.println((end - start) + " ms");
     }
 
 
